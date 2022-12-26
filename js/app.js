e0637b1680dd7773b55154a317c53640c4666fc7
romain2518 <rolenoir25@gmail.com>
2022-12-26 22:43:16 +0100

    Add scroll snap fix as the bottom of the page was unreachable

--- js/app.js
+++ js/app.js
@@ -3,9 +3,11 @@ import contactForm from "./components/contactForm";
 import filterSystem from "./components/filterSystem";
 import mainSlider from "./components/mainSlider";
 import projectList from "./components/projectList";
+import scrollSnapFixer from "./components/scrollSnapFixer";
 
 const app = {
     init: function () {
+        scrollSnapFixer.init();
         mainSlider.init();
         about.init();
         contactForm.init();
