cab7801a58d1578c7a660fd466ddcb87cfa159ff
romain2518 <rolenoir25@gmail.com>
2022-12-30 18:32:31 +0100

    Fix projects were sorted from oldest to newest instead of the new->old

--- js/components/dataLoader.js
+++ js/components/dataLoader.js
@@ -10,9 +10,9 @@ const dataLoader = {
         // Sort projects by date from newest to oldest
         data.projects.sort((a, b) => {
             // a is newer than b
-            if (a.date > b.date) return 1;
+            if (a.date > b.date) return -1;
             // a is older than b
-            if (a.date < b.date) return -1;
+            if (a.date < b.date) return 1;
             // a & b are equals
             return 0;
         });
