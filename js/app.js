91bb3401cea2781fb0366a8892de08c3230d6729
romain2518 <rolenoir25@gmail.com>
2023-07-04 20:42:42 +0200

    Add up button

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ import filterSystem from "./components/filterSystem.js";
 import mainSlider from "./components/mainSlider.js";
 import projectList from "./components/projectList.js";
 import projectModal from "./components/projectModal.js";
+import upButton from "./components/upButton.js";
 
 const app = {
     init: function () {
@@ -14,6 +15,7 @@ const app = {
         about.init();
         contactForm.init();
         filterSystem.init();
+        upButton.init();
         projectList.init();
         projectModal.init();
 
